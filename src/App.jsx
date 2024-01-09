72ceb8e311498ea343f7791320fe61b2618e23c9
Gina Alfandary Magana <ginaleealfy@gmail.com>
2024-01-09 12:59:26 -0800

    removed unnecessary variable

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ import {
 
 const App = () => {
   const [display, setDisplay] = useState("");
-  const [currentAnswerDisplay, setCurrentAnswerDisplay] = useState("0");
   const [currentAnswer, setCurrentAnswer] = useState("0");
   const [lastNumber, setLastNumber] = useState("");
   const [operator, setOperator] = useState("");
@@ -32,26 +31,21 @@ const App = () => {
     if (num === ".") {
       if (isCalculated || isOperatorSet) {
         setCurrentAnswer("0.");
-        setCurrentAnswerDisplay("0.");
         setIsDecimal(true);
       } else if (!isDecimal) {
         setCurrentAnswer(currentAnswer + num);
-        setCurrentAnswerDisplay(currentAnswer + num);
         setIsDecimal(true);
       }
     } else {
       if (isClear) {
         setCurrentAnswer(num);
-        setCurrentAnswerDisplay(num);
       } else if (isCalculated || (isOperatorSet && !isLastInputNumber)) {
         setLastNumber(currentAnswer);
         setCurrentAnswer(num);
-        setCurrentAnswerDisplay(num);
       } else if (currentAnswer === "0") {
         return;
       } else {
         setCurrentAnswer(currentAnswer + num);
-        setCurrentAnswerDisplay(currentAnswer + num);
       }
     }
     setIsLastInputNumber(true);
@@ -110,21 +104,18 @@ const App = () => {
 
     if (total === "Cannot Divide by 0") {
       setDisplay(`${lastNumber} ${operatorText}`);
-      setCurrentAnswer("0");
       setIsClearNeeded(true);
     } else {
       setLastNumber(total);
       setDisplay(`${total} ${operatorText}`);
-      setCurrentAnswer(total);
     }
+    setCurrentAnswer(total);
 
-    setCurrentAnswerDisplay(total);
     setIsCalculated(true);
   };
 
   const clearCalculator = () => {
     setDisplay("");
-    setCurrentAnswerDisplay("0");
     setCurrentAnswer("0");
     setLastNumber("");
     setOperator("");
@@ -203,7 +194,7 @@ const App = () => {
 
   return (
     <div className="container">
-      <Screen display={display} currentAnswerDisplay={currentAnswerDisplay} />
+      <Screen display={display} currentAnswer={currentAnswer} />
       <div className="buttons" onClick={grabInfo}>
         <div className="row">
           <button className="clear-entry">
--- src/Screen.jsx
+++ src/Screen.jsx
@@ -1,8 +1,8 @@
-const Screen = ({ display, currentAnswerDisplay }) => {
+const Screen = ({ display, currentAnswer }) => {
   return (
     <div className="screen">
       <p>{display}</p>
-      <h1>{currentAnswerDisplay}</h1>
+      <h1>{currentAnswer}</h1>
     </div>
   );
 };
